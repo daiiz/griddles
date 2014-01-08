1ffe0b619083c0df9da343d4e8303f5745387abb
daiz <da.iz.mg@gmail.com>
2014-01-08 23:42:28 +0900

    ファイル追加

---
+++ demo.js
@@ -0,0 +1,45 @@
+﻿var demo = {
+   menu: ["ようこそ！", "ふせん"],
+   load: function() {
+      var e = 0;
+      var opt = "";
+      for(e = 0; e < demo.menu.length; e++) {
+         opt = opt + '<option value="'+ demo.menu[e] +'">'+ demo.menu[e] +'</option>';
+      }
+      d.getElementById("select_menu").innerHTML = opt;
+      d.getElementById("select_menu").addEventListener("change", demo.change, false);
+   },
+   change: function(e) {
+      console.log(e.target.value);
+      griddles.render = true;
+      switch(e.target.value) {
+         case "ようこそ！" : 
+            layout.card_height_px = 250;
+            layout.card_width_px = 250;
+            layout.cards = [
+               {"id": "c0", "type": "user-img", "dataset": [], "init": "icon.png"},
+               {"id": "c1", "type": "user-text", "dataset": [], "init": "Hello, griddles!"},
+               {"id": "c2", "type": "user-free", "dataset": [], "init": "画面の大きさに合わせて<br>カードがセンタリングされます！"}
+            ];
+            griddles.load();
+            break;
+         case "ふせん" : 
+            layout.card_height_px = "auto";
+            layout.card_width_px = 250;
+            layout.cards = [
+               {"id": "c0", "type": "my-husen-blue", "dataset": [], "init": "<div class=c>ふせん</div>"},
+               {"id": "c1", "type": "my-husen-yellow", "dataset": [], "init": "<div class=c>ふ<br>せ<br>ん</div>"},
+               {"id": "c6", "type": "my-husen-gray", "dataset": [], "init": "<div class='c y'>￥ 0 </div>"},
+               {"id": "c4", "type": "my-husen-gray", "dataset": [], "init": "<div class=c><img src='daiz.png' style='width:150px'></div>"},
+               {"id": "c2", "type": "my-husen-red", "dataset": [], "init": "<div class=c>いろいろな<br>サイズが<br>あっても<br>大丈夫！</div>"},
+               {"id": "c3", "type": "my-husen-green", "dataset": [], "init": "<div class=c>ふせん</div>"},
+               {"id": "c5", "type": "my-husen-yellow", "dataset": [], "init": "<div class=c>スマートフォンにも対応</div>"}
+            ];
+            griddles.load();
+            break;
+      }
+   }
+};
+
+demo.load();
+
---
+++ settings.js
@@ -0,0 +1,17 @@
+﻿var layout = {
+   "page_title": "sample",
+   "app_name": "Sample",
+   "app_icon": "icon.png",
+   "card_width_px": 250,
+   "card_height_px": 250, /* "auto" */
+   "card_margin_bottom": 14,
+   "stream_margin_left_px": 7,
+   "stream_margin_right_px": 7,
+   "available_width_percent": 95,
+   "tooltip": "no",
+   "cards": [
+      {"id": "c0", "type": "user-img", "dataset": [], "init": "icon.png"},
+      {"id": "c1", "type": "user-text", "dataset": [], "init": "Hello, griddles!"},
+      {"id": "c2", "type": "user-free", "dataset": [], "init": "画面の大きさに合わせて<br>カードがセンタリングされます！"}
+   ]
+};
