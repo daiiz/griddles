d3bf1e0f6f8b793495cc8979866b1b4e57c8e2d9
daiz <da.iz.mg@gmail.com>
2014-04-02 01:01:24 +0900

    Nightly

---
+++ dev/assets/griddles-manifest.js
@@ -0,0 +1,39 @@
+﻿/**
+ * Griddles v0.0.23b
+ * (c) 2013-2014 daiz. https://github.com/daiz713/griddles
+ * License: MIT
+ */
+
+var griddles = griddles || {};
+
+ /* User settings */
+griddles.msgLoading = "+griddles";
+
+/* ３つのうちどれか一つをtrueにする */
+griddles.chromeApp = false;
+griddles.phonegap = false;
+griddles.webPage = true;
+
+griddles.layout = {
+    "page_title": "griddles",
+    "app_icon": "icon.png",
+    "app_bar_color": "#000",       //New!
+    "app_bar_bg_color": "#fff",    //New!
+    "background_color": "#E5E5E5",
+    "card_width_px": 250, //360
+    "card_height_px": 250,/* OR "auto" */
+    "card_paddings": [6, 6, 6, 6],    //New!
+    "card_margin_bottom": 18,
+    "stream_margin_left_px": 9,
+    "stream_margin_right_px": 9,
+    "available_width_percent": 95,
+    "max_streams_limit": 2,   //New!
+    "card_tooltip": false,          //Changed! true OR false
+    "load_limit": 2,
+    "cards": [
+    ]
+};
+
+griddles.layout.cardOnClick = function(j) {
+    console.log(j);
+}
---
+++ dev/myapp.js
@@ -0,0 +1,61 @@
+﻿// This is a sample.
+
+var myApp = myApp || {};
+
+myApp.load = function(e) {
+   myApp.settingSelectBox(["fixed", "auto"]);
+   var arr = arr || [];
+   arr.push({"wide": true, "id": "msg0", "type": "user-text", "dataset": [["hoge","hoge"]], "init": "Hello Griddles!", card: "#EEEE22", paddings: [26, 26, 26, 26]});
+   arr.push({"id": "author", "type": "user-caption-img", "dataset": [["hoge","hoge"]], "init": "https://identicons.github.com/40b2bcc96299804ade6b438e85a0bb0e.png", "card": "#fff", "caption": "By daiz.", "caption_height_px": 30});
+   arr.push({"wide": true, "id": "author1", "type": "user-caption-img", "dataset": [["hoge","hoge"]], "init": "https://identicons.github.com/40b2bcc96299804ade6b438e85a0bb0e.png", "card": "#fff", "caption": "Hello!", "caption_height_px": 30});
+   arr.push({"id": "a0", "type": "user-caption-img", "dataset": [["hoge","hoge"]], "init": "sample-images/1.png", "card": false, "caption": "キャプション1（台紙なし）", "caption_height_px": 30, paddings: [0, 0, 0, 0]});
+   arr.push({"id": "a1", "type": "user-img", "dataset": [["hoge","hoge"]], "init": "sample-images/2.png"});
+   arr.push({"id": "a2", "type": "user-img", "dataset": [["hoge","hoge"]], "init": "sample-images/3.png"});
+   arr.push({"id": "a3", "type": "user-img", "dataset": [["hoge","hoge"]], "init": "sample-images/4.png"});
+   arr.push({"id": "a4", "type": "user-img", "dataset": [["hoge","hoge"]], "init": "sample-images/5.png"});
+   arr.push({"id": "msg1", "type": "user-text", "dataset": [["hoge","hoge"]], "init": "<b>Javascript!</b>", card: "#FFBB22", paddings: [36, 36, 36, 36]});
+   arr.push({"wide": true, "id": "a5", "type": "user-img", "dataset": [["hoge","hoge"]], "init": "sample-images/6.png"});
+   arr.push({"id": "a6", "type": "user-img", "dataset": [["hoge","hoge"]], "init": "sample-images/7.png"});
+   arr.push({"id": "a7", "type": "user-img", "dataset": [["hoge","hoge"]], "init": "sample-images/8.png"});
+   arr.push({"id": "msg2", "type": "user-text", "dataset": [["hoge","hoge"]], "init": "つくりたいものをつくる", card: "#BBEE33"});
+   arr.push({"id": "a8", "type": "user-caption-img", "dataset": [["hoge","hoge"]], "init": "sample-images/9.png", "caption": "キャプション9", "caption_height_px": 30, "card": true});
+   arr.push({"id": "a9", "type": "user-img", "dataset": [["hoge","hoge"]], "init": "sample-images/10.png"});
+   arr.push({"wide": true, "id": "a10", "type": "user-img", "dataset": [["hoge","hoge"]], "init": "sample-images/11.png"});
+   arr.push({"id": "a11", "type": "user-img", "dataset": [["hoge","hoge"]], "init": "sample-images/12.png"});
+   arr.push({"id": "a12", "type": "user-img", "dataset": [["hoge","hoge"]], "init": "sample-images/13.png"});
+   arr.push({"id": "a13", "type": "user-img", "dataset": [["hoge","hoge"]], "init": "sample-images/13.png"});
+   arr.push({"id": "msg3", "type": "user-text", "dataset": [["hoge","hoge"]], "init": "<a href='https://github.com/daiz713/griddles' target='_blank'>View on GitHub</a>"});
+   myApp.renderingCards(arr);
+}
+
+myApp.changedSelectBox = function(e) {
+   var val = e.target.value;
+   switch(val) {
+     case "fixed": griddles.layout.card_height_px = 250;griddles.render = true;griddles.load();break;
+     case "auto": griddles.layout.card_height_px = "auto";griddles.render = true;griddles.load();break;
+   }
+}
+
+myApp.settingSelectBox = function(options) {
+    /* e.g. options == ["a", "b", "c"] */
+    var optionTags = "";
+    for(var i = 0; i < options.length; i++) {
+       var opt = options[i];
+       optionTags = optionTags + '<option value="'+ opt +'">'+ opt +'</option>';
+    }
+    document.getElementById("select_menu").innerHTML = optionTags;
+    document.getElementById("select_menu").addEventListener("change", myApp.changedSelectBox, false);
+}
+
+myApp.renderingCards = function(cards) {
+   /* e.g
+    * cards == [{"id": "i0", "type": "user-img", "dataset": ["name", "A"], "init": "a.png"},
+    *           {"id": "i1", "type": "user-img", "dataset": ["name", "B"], "init": "b.png"},
+    *           {"id": "i2", "type": "user-img", "dataset": ["name", "C"], "init": "c.png"}]
+    */
+   griddles.layout.cards = cards;
+   griddles.render = true;  /* important! */
+   griddles.load();
+}
+
+window.addEventListener("load", myApp.load, false);
